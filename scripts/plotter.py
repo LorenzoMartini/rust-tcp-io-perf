bab246de8ea04701cacb62fc0f7703060cb5a1fc
LorenzoMartini <wiismart@hotmail.it>
2018-11-23 11:42:49 +0100

    Specialize plotter

--- scripts/plotter.py
+++ scripts/plotter.py
@@ -9,17 +9,17 @@ import sys
 
 # Plot given measurments samples
 def plot_measurements(measurements, program):
-    # TODO add cdf and specialize
+    # TODO add cdf
     x_axis = np.arange(0, len(measurements))
-    gbs = []
+    y_axis = []
     for sample in measurements:
-        gbs.append(sample.n_bytes / sample.time_us)
+        y_axis.append((sample.n_bytes / sample.time_us) if program == 'rust-tcp-bw' else sample.time_us)
 
-    plt.plot(x_axis, gbs)
+    plt.plot(x_axis, y_axis)
     plt.grid(True)
-    plt.title('Network effective bandwidth')
+    plt.title('Network effective bandwidth' if program == 'rust-tcp-bw' else 'Latency')
     plt.xlabel('Sample #')
-    plt.ylabel('MB/s')
+    plt.ylabel('MB/s' if program == 'rust-tcp-bw' else 'us')
     plt.show()
 
 
