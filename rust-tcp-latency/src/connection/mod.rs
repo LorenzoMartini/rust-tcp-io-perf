b52001cbb3ddd66256256fe9dd499a2ecc4c46f3
LorenzoMartini <wiismart@hotmail.it>
2018-11-30 10:58:49 +0100

    Edit comment

--- rust-tcp-latency/src/connection/mod.rs
+++ rust-tcp-latency/src/connection/mod.rs
@@ -1,7 +1,8 @@
 use std::net::TcpStream;
 use std::io::{Read, Write};
 
-/// Sends first n_bytes from wbuf using the given stream
+/// Sends first n_bytes from wbuf using the given stream.
+///// Make sure wbuf.len >= n_bytes
 pub fn send_message(n_bytes: usize, stream: &mut TcpStream, wbuf: &Vec<u8>) {
     let mut send = 0;
     while send < n_bytes {
@@ -15,7 +16,8 @@ pub fn send_message(n_bytes: usize, stream: &mut TcpStream, wbuf: &Vec<u8>) {
     }
 }
 
-/// Reads n_bytes into rbuf from the given stream
+/// Reads n_bytes into rbuf from the given stream.
+/// Make sure rbuf.len >= n_bytes
 pub fn receive_message(n_bytes: usize, stream: &mut TcpStream, rbuf: &mut Vec<u8>) {
     // Make sure we receive the full buf back
     let mut recv = 0;
