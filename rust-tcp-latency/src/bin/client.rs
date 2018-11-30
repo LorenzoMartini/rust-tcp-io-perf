933a5efb65b87cca0a5ff11c192290020674656a
LorenzoMartini <wiismart@hotmail.it>
2018-11-30 10:12:17 +0100

    Make non-blocking

--- rust-tcp-latency/src/bin/client.rs
+++ rust-tcp-latency/src/bin/client.rs
@@ -11,6 +11,23 @@ fn print_line() {
     println!("\n-------------------------------------------------------------\n");
 }
 
+
+fn print_summary(hist: streaming_harness_hdrhist::HDRHist) {
+    println!("Sent/received everything!");
+    print_line();
+    println!("HDRHIST summary, measure in ns");
+    print_line();
+    println!("summary:\n{:#?}", hist.summary().collect::<Vec<_>>());
+    print_line();
+    println!("Summary_string:\n{}", hist.summary_string());
+    print_line();
+    println!("CDF summary:\n");
+    for entry in hist.ccdf() {
+        println!("{:?}", entry);
+    }
+}
+
+
 fn main() {
 
     let args = config::parse_config();
@@ -18,7 +35,7 @@ fn main() {
     println!("Connecting to the server {}...", args.address);
     let n_rounds = args.n_rounds;
     let n_bytes = args.n_kbytes * 1000;
-    let mut measurements = Vec::new();
+    let mut hist = streaming_harness_hdrhist::HDRHist::new();
 
     // Create buffers to read/write
     let wbuf = vec![0; n_bytes];
@@ -27,21 +44,40 @@ fn main() {
     let progress_tracking_percentage = n_rounds / 100;
 
     if let Ok(mut stream) = TcpStream::connect(args.address_and_port()) {
+        stream.set_nodelay(true).expect("Can't set no_delay to true");
+        stream.set_nonblocking(true).expect("Can't set channel to be non-blocking");
+
         println!("Connection established! Ready to send...");
 
         for i in 0..n_rounds {
+
+            // Make sure we send everything
+            let mut send = 0;
             let start = Instant::now();
-            match stream.write(&wbuf) {
-                Ok(_n) => {},
-                Err(err) => panic!("crazy stuff happened while sending {}", err),
+            while send < n_bytes {
+                match stream.write(&wbuf) {
+                    Ok(n) => send += n,
+                    Err(err) => match err.kind() {
+                        std::io::ErrorKind::WouldBlock => {}
+                        _ => panic!("Error occurred while writing: {:?}", err),
+                    }
+                }
             }
 
             // Make sure we receive the full buf back
             let mut recv = 0;
             while recv < n_bytes {
-                recv += stream.read(&mut rbuf).unwrap();
+                match stream.read(&mut rbuf){
+                    Ok(n) => recv += n,
+                    Err(err) => match err.kind() {
+                        std::io::ErrorKind::WouldBlock => {}
+                        _ => panic!("Error occurred while reading: {:?}", err),
+                    }
+                }
             }
-            measurements.push(Instant::now().duration_since(start));
+
+            let duration = Instant::now().duration_since(start);
+            hist.add_value(duration.as_secs() * 1_000_000_000u64 + duration.subsec_nanos() as u64);
 
             if i % progress_tracking_percentage == 0 {
                 // Track progress on screen
@@ -51,24 +87,9 @@ fn main() {
 
         stream.shutdown(Shutdown::Both).expect("shutdown call failed");
 
-        println!("Sent/received everything!");
-        let mut hist = streaming_harness_hdrhist::HDRHist::new();
-        for measure in measurements {
-            hist.add_value(measure.as_secs() * 1_000_000_000u64 + measure.subsec_nanos() as u64);
-        }
-
         // Format output nicely
-        print_line();
-        println!("HDRHIST summary, measure in ns");
-        print_line();
-        println!("summary:\n{:#?}", hist.summary().collect::<Vec<_>>());
-        print_line();
-        println!("Summary_string:\n{}", hist.summary_string());
-        print_line();
-        println!("CDF summary:\n");
-        for entry in hist.ccdf() {
-            println!("{:?}", entry);
-        }
+        print_summary(hist);
+
     } else {
         println!("Couldn't connect to server...");
     }
--- rust-tcp-latency/src/bin/server.rs
+++ rust-tcp-latency/src/bin/server.rs
@@ -15,18 +15,37 @@ fn main() {
 
     println!("Server running, listening for connection on 0.0.0.0:{}", &args.port);
     let mut stream = listener.incoming().next().unwrap().unwrap();
+    stream.set_nodelay(true).expect("Can't set no_delay to true");
+    stream.set_nonblocking(true).expect("Can't set channel to be non-blocking");
 
     println!("Connection established with {:?}!\nExpected {} Bytes for {} rounds",
              stream.peer_addr().unwrap(), n_bytes, args.n_rounds);
 
+    // Make sure n_rounds is the same between client and server
     for _i in 0..n_rounds {
+
+        // Read
         let mut recv = 0;
         while recv < n_bytes {
-            recv += stream.read(&mut buf).unwrap();
+            match stream.read(&mut buf){
+                Ok(n) => recv += n,
+                Err(err) => match err.kind() {
+                    std::io::ErrorKind::WouldBlock => {}
+                    _ => panic!("Error occurred while reading: {:?}", err),
+                }
+            }
         }
-        match stream.write(&buf) {
-            Ok(_n) => {},
-            Err(err) => panic!("crazy stuff happened while sending {}", err),
+
+        // Send back
+        let mut send = 0;
+        while send < n_bytes {
+            match stream.write(&buf) {
+                Ok(n) => send += n,
+                Err(err) => match err.kind() {
+                    std::io::ErrorKind::WouldBlock => {}
+                    _ => panic!("Error occurred while writing: {:?}", err),
+                }
+            }
         }
     }
     println!("Done exchanging stuff")
